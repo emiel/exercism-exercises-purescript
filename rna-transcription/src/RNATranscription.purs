a5b65d7e8d8153a3529d9b84039f3d4a8a5ab92c
Emiel van de Laar <gemiel@gmail.com>
2020-11-29 14:30:48 +0100

    Exercise: RNA Transcription

--- rna-transcription/src/RNATranscription.purs
+++ rna-transcription/src/RNATranscription.purs
@@ -1,18 +1,29 @@
-module RNATranscription
-  ( toRNA
-  ) where
+module RNATranscription (toRNA) where
 
 import Prelude
-import Data.String.CodeUnits
-import Data.Maybe
+import Data.Maybe (Maybe(..))
+import Data.String.CodeUnits (toCharArray, fromCharArray)
+import Data.Traversable (traverse)
 
-transcribe :: Char -> Maybe Char
-transcribe c = case c of
+type Nucleotide
+  = Char
+
+type DNA
+  = Array Nucleotide
+
+type RNA
+  = Array Nucleotide
+
+complement :: Nucleotide -> Maybe Nucleotide
+complement n = case n of
   'A' -> Just 'U'
   'C' -> Just 'G'
   'G' -> Just 'C'
   'T' -> Just 'A'
   _ -> Nothing
 
--- toRNA :: String -> Maybe String
-toRNA str = (toCharArray str) >>= transcribe
+toRNA :: String -> Maybe String
+toRNA strand = fromCharArray <$> transcribe (toCharArray strand)
+  where
+  transcribe :: DNA -> Maybe RNA
+  transcribe = traverse complement
