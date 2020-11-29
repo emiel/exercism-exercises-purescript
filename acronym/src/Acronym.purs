10ce51b0599815e29e3a6d54751ff88bd958256f
Emiel van de Laar <gemiel@gmail.com>
2020-11-29 14:32:15 +0100

    Exercise: Acronym

--- acronym/src/Acronym.purs
+++ acronym/src/Acronym.purs
@@ -1,13 +1,23 @@
 module Acronym (abbreviate) where
 
 import Prelude
+import Data.Array (take, filter)
+import Data.Char.Unicode (isAlpha, isUpper, isLower, toUpper)
+import Data.Foldable (all)
+import Data.String (split, Pattern(..), joinWith)
+import Data.String.CodeUnits (fromCharArray, toCharArray)
 
-import Data.String (Pattern(..), split)
-import Data.String.CodeUnits (charAt, fromCharArray)
+acronym :: Array Char -> Array Char
+acronym cs
+  | all isUpper cs = take 1 cs
+  | all isLower cs = toUpper <$> take 1 cs
+  | otherwise = filter isUpper cs
 
--- abbreviate :: String -> String
-abbreviate s =
-  let
-    words = split (Pattern " ") s
-  in
-    ?asdf (charAt 1) <$> words
+abbreviate :: String -> String
+abbreviate xs = joinWith "" $ fromCharArray <$> acronym <<< normalizeChars <<< toCharArray <$> words xs
+  where
+  words :: String -> Array String
+  words = split (Pattern " ")
+
+  normalizeChars :: Array Char -> Array Char
+  normalizeChars cs = filter isAlpha cs
